67ed5974b47e655919ae4929d34d54a6ba6f2a49
Sameeraja Shyam <79054143+sameerajashyam@users.noreply.github.com>
2025-03-27 15:04:23 +0530

    Update setup.py

--- Zomato-Chatbot/setup.py
+++ Zomato-Chatbot/setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages,setup
 
 setup(name="Generative AI Project",
       version='0.0.0',
-      author='Bappy',
-      author_email='entbappy73@gmail.com',
+      author='Name of author',
+      author_email='mail@gmail.com',
       packages=find_packages(),
       install_requires=[]
       
